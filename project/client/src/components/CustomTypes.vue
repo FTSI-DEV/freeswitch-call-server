5a0259375119e63c56ae62ab97909539da651bbe
jsonbasarte <jbasarte@mhdevteam.com>
2021-08-06 13:26:54 +0800

    added axios and base api service

--- project/client/src/components/CustomTypes.vue
+++ project/client/src/components/CustomTypes.vue
@@ -10,23 +10,47 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, PropType } from "vue";
 import EventsService  from '../services/EventService';
+import { EventItem } from '../types/EventItemType';
 export default defineComponent({
   name: "HelloWorld",
   props: {
-    msg: String,
+    // Props typing ( custom type )
+    event: {
+      type: Object as PropType<EventItem>,
+      required: true
+    }
   },
   data() {
     return {
-      events: null
+      // data typing ( custom type )
+      //events: {} as EventItem,
+      events: [] as EventItem[],
+    }
+  },
+  computed: {
+    // returning CUSTOM TYPE
+    // Computed property typing
+    firstEvent(): EventItem {
+      return this.events[0]
     }
   },
   methods: {
     async getEvents() {
+      console.log('first');
       const events = await EventsService.getEvents();
       console.log('events', events.data);
-    }
+      console.log('second');
+    },
+    // parameter typing ( custom type )
+    addEvent(newEvent: EventItem) {
+      this.events.push(newEvent)
+    },
+    // return typing ( custom type )
+    secondEvent(): EventItem {
+      return this.events[1]
+    },
   },
   created() {
     this.getEvents();
@@ -36,7 +60,7 @@ export default defineComponent({
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped>  
 h3 {
   margin: 40px 0 0;
 }
