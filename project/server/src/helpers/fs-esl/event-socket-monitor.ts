c8b0712fcbf5cb12f493ef3741efc9c56375ef44
Venalyn Sudaria <vsudaria@mhdevteam.com>
2021-08-24 11:05:50 +0800

    update

--- project/server/src/helpers/fs-esl/event-socket-monitor.ts
+++ project/server/src/helpers/fs-esl/event-socket-monitor.ts
@@ -1,4 +1,4 @@
-import { ALL_EVENTS, DTMF_EVENTS, FS_ESL } from "../constants/freeswitch.constants";
+import { ALL_EVENTS, DTMF_EVENTS, FS_ESL } from "../constants/fs-esl.constants";
 import { CallDispatchHelper } from "./callDispatch.helper";
 import { CDRHelper } from "./cdr.helper";
 import { FreeswitchConnectionHelper, fsConnect } from "./eslfreeswitch.connection";
@@ -8,10 +8,14 @@ export class StartFreeswitchApplication {
     private readonly _fsConnection = new FreeswitchConnectionHelper();
 
     startFS():any{
-        this._fsConnection.connect().then((connection) => {
+        let ff = this._fsConnection.connect().then((connection) => {
             console.log('trying to subscribe to the event: ', connection.event);
 
-            new CallDispatchHelper().clickToCall2(connection, '1000', '4001', 'vena1001')
+            new CallDispatchHelper().clickToCall(connection, {
+                phoneNumberFrom: '1000',
+                phoneNumberTo : '1000',
+                storeId: 60
+            });
             //intercepting calls
             connection.on(FS_ESL.RECEIVED, fsEvent => {
                new CDRHelper().getCallRecords(fsEvent);
