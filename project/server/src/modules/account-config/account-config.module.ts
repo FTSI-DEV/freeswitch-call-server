a23f0958c845762762f15fc9819a67dd1abdbb3f
Venalyn Sudaria <vsudaria@mhdevteam.com>
2021-10-05 13:19:59 +0800

    register controlller

--- project/server/src/modules/account-config/account-config.module.ts
+++ project/server/src/modules/account-config/account-config.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountConfigEntity, AccountConfigEntityRepository } from 'src/entity/account-config';
+import { AccountConfigController } from './controllers/account-config.controller';
 import { ACCOUNT_CONFIG_SERVICE } from './services/account-config.interface';
 import { AccountConfigService } from './services/account-config.service';
 
@@ -12,6 +13,7 @@ import { AccountConfigService } from './services/account-config.service';
         }
     ],
     imports: [TypeOrmModule.forFeature([AccountConfigEntity, AccountConfigEntityRepository])],
-    exports: [ACCOUNT_CONFIG_SERVICE]
+    exports: [ACCOUNT_CONFIG_SERVICE],
+    controllers: [AccountConfigController]
 })
 export class AccountConfigModule {}
